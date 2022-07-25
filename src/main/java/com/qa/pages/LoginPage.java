74c2830b47964c4f84a3f3ac6e20aac3158ee374
sanketsborade <ssanketborade@gmail.com>
2022-07-25 11:03:06 +0530

    third commit

--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -1,5 +1,9 @@
 package com.qa.pages;
 
 public class LoginPage {
+public void loginPage() {
+	System.out.println("this is login page method");
+	System.out.println("this is new login page method");
 
+}
 }
